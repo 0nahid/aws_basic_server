a5f724d3b109796e1bb077d08739dd75961c57e6
0nahid <nahidhassanbulbul@gmail.com>
2023-11-01 12:58:14 +0600

    server2

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World from server 1'))
-app.listen(port, () => console.log(`Example app 1 listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World from server 2'))
+app.listen(port, () => console.log(`Example app 2 listening on port ${port}!`))
